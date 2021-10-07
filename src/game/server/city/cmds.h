58b5adad77f55ab4ed064750f6addc12f4116028
ST-Chara <chara_kill_sans@163.com>
2021-10-07 10:32:34 +0800

    upload uTown

---
+++ src/game/server/city/cmds.h
@@ -0,0 +1,24 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+//const char *itob(char *input);
+//void chat_command(CNetMsg_Cl_Say *msg, CPlayer *p, CGameContext *game);
+//void BuyItem(int *AccItem, char ItemName[128], int Price, int Money);
+#ifndef GAME_SERVER_CMD_H
+#define GAME_SERVER_CMD_H
+#include "game/server/gamecontext.h"
+
+class CCmd
+{
+public:
+	CCmd(CPlayer *pPlayer, CGameContext *pGameServer);
+	void ChatCmd(CNetMsg_Cl_Say *Msg);
+
+private:
+	CPlayer *m_pPlayer;
+	CGameContext *m_pGameServer;
+	CGameContext *GameServer() const { return m_pGameServer; }
+
+	void LastChat();
+};
+
+#endif
---
+++ src/game/server/gamemodes/dm.cpp
@@ -0,0 +1,17 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+﻿/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+#include "dm.h"
+
+
+CGameControllerDM::CGameControllerDM(class CGameContext *pGameServer)
+: IGameController(pGameServer)
+{
+	m_pGameType = "uTown";
+}
+
+void CGameControllerDM::Tick()
+{
+	IGameController::Tick();
+}
