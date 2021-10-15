ae4fe510cc449140800e74b00e394ac3b718f1c0
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2021-10-15 13:44:49 +0800

    Update server.cpp

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -168,7 +168,7 @@ void CServer::CClient::Reset()
 	m_LastInputTick = -1;
 	m_SnapRate = CClient::SNAPRATE_INIT;
 	m_Score = 0;
-	m_AccID = -1
+	m_AccID = -1;
 }
 
 CServer::CServer() : m_DemoRecorder(&m_SnapshotDelta)
